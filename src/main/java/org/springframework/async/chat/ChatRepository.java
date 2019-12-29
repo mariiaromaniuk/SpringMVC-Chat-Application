9b45ddcb76c65e3faf3bc3fccb43d5f6b14c1be9
mariiaromaniuk <41880331+mariiaromaniuk@users.noreply.github.com>
2019-12-29 01:20:40 -0500

    Add files via upload

---
+++ src/main/java/org/springframework/async/chat/ChatRepository.java
@@ -0,0 +1,11 @@
+package org.springframework.async.chat;
+
+import java.util.List;
+
+public interface ChatRepository {
+
+	List<String> getMessages(int messageIndex);
+
+	void addMessage(String message);
+
+}
